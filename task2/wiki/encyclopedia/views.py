68827142789fae0d92110d4fcbf2efcdb1b18d82
magari-GH <magari.study@gmail.com>
2023-08-21 22:24:10 +0200

    Added new function for convert and display files.md

--- task2/wiki/encyclopedia/views.py
+++ task2/wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
+import markdown2
 from django.shortcuts import render
+from markdown2 import markdown
 
 from . import util
 
@@ -8,3 +10,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry_f(request, title):
+    title_name = f"{title.capitalize()}"
+    text_html = util.get_entry(title)
+    if text_html is None:
+        text_html = f"Page {title.capitalize()} has not been found!"
+    else:
+        text_html = markdown(open(f"entries/{title}.md").read())
+    return render(request, "encyclopedia/title.html", {
+            "title_name": title_name,
+            "text_html": text_html
+})
