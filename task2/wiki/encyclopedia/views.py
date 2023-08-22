6eb030cb18f9b29c561651fc2588f6c1c898445c
magari-GH <magari.study@gmail.com>
2023-08-22 12:24:03 +0200

    Add random search

--- task2/wiki/encyclopedia/views.py
+++ task2/wiki/encyclopedia/views.py
@@ -1,13 +1,18 @@
 import markdown2
 from django.shortcuts import render
 from markdown2 import markdown
-
 from . import util
+from django import forms
+
+
+class SearchForm(forms.Form):
+    input_text = forms.CharField(label="Search Encyclopedia")
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
 
@@ -20,5 +25,15 @@ def entry_f(request, title):
         text_html = markdown(open(f"entries/{title}.md").read())
     return render(request, "encyclopedia/title.html", {
             "title_name": title_name,
-            "text_html": text_html
+            "text_html": text_html,
+            "form": SearchForm(),
+            "entries": util.list_entries()
 })
+
+
+
+
+    #if request.method == "GET":
+       # form = SearchForm(request.GET)
+      #  if form.is_valid():
+        #    title = form.cleaned_data["title"]
