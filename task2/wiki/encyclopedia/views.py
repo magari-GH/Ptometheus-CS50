345959a8b48ad542a7f62038a5faa6adb3777fbf
magari-GH <magari.study@gmail.com>
2023-08-23 22:05:27 +0200

    Added new function 'search' for layout search by HTML form (dynamical url)

--- task2/wiki/encyclopedia/views.py
+++ task2/wiki/encyclopedia/views.py
@@ -1,22 +1,15 @@
-import markdown2
 from django.shortcuts import render
 from markdown2 import markdown
 from . import util
-from django import forms
-
-
-class SearchForm(forms.Form):
-    input_text = forms.CharField(label="Search Encyclopedia")
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
-        "form": SearchForm()
     })
 
 
-def entry_f(request, title):
+def title(request, title):
     title_name = f"{title.capitalize()}"
     text_html = util.get_entry(title)
     if text_html is None:
@@ -26,14 +19,21 @@ def entry_f(request, title):
     return render(request, "encyclopedia/title.html", {
             "title_name": title_name,
             "text_html": text_html,
-            "form": SearchForm(),
             "entries": util.list_entries()
 })
+ 
 
+def search(request):
+    title1 = request.GET.get("search_box")
+    title_name = f"{title1.capitalize()}"
+    text_html = util.get_entry(title1)
+    if text_html is None:
+        text_html = f"Page {title1.capitalize()} has not been found!"
+    else:
+        text_html = markdown(open(f"entries/{title1}.md").read())
+    return render(request, "encyclopedia/title.html", {
+            "title_name": title_name,
+            "text_html": text_html,
+            "entries": util.list_entries()
 
-
-
-    #if request.method == "GET":
-       # form = SearchForm(request.GET)
-      #  if form.is_valid():
-        #    title = form.cleaned_data["title"]
+    })
