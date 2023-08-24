e7952847d075f3f8a3772a7e32578dd2bdaf60fd
magari-GH <magari.study@gmail.com>
2023-08-24 15:08:27 +0200

    Added new function for search use instance of forms.Form

--- task2/wiki/encyclopedia/views.py
+++ task2/wiki/encyclopedia/views.py
@@ -1,39 +1,86 @@
 from django.shortcuts import render
 from markdown2 import markdown
 from . import util
+from django import forms
+
+
+class NewSearchForm(forms.Form):
+    text_form = forms.CharField(label="Search Encyclopedia:")
+
+
+entries = util.list_entries()
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
+        "form": NewSearchForm(),
     })
 
 
 def title(request, title):
-    title_name = f"{title.capitalize()}"
+    title_name = f"{title}"
     text_html = util.get_entry(title)
     if text_html is None:
-        text_html = f"Page {title.capitalize()} has not been found!"
+        text_html = f"Page {title} has not been found!"
     else:
         text_html = markdown(open(f"entries/{title}.md").read())
     return render(request, "encyclopedia/title.html", {
             "title_name": title_name,
             "text_html": text_html,
-            "entries": util.list_entries()
-})
- 
+            "entries": util.list_entries(),
+            "form": NewSearchForm(),
+    })
 
-def search(request):
+
+def search_html(request):
     title1 = request.GET.get("search_box")
-    title_name = f"{title1.capitalize()}"
+    title_name = f"{title1}"
     text_html = util.get_entry(title1)
     if text_html is None:
-        text_html = f"Page {title1.capitalize()} has not been found!"
+        lists = []
+        for i in util.list_entries():
+            if title1 in i:
+                lists.append(i)
+        return render(request, "encyclopedia/search.html", {
+                    "form": NewSearchForm(),
+                    "lists": lists,
+                    "entries": entries,
+                    "title1": title1})
     else:
         text_html = markdown(open(f"entries/{title1}.md").read())
-    return render(request, "encyclopedia/title.html", {
+        return render(request, "encyclopedia/title.html", {
             "title_name": title_name,
             "text_html": text_html,
-            "entries": util.list_entries()
-
+            "entries": util.list_entries(),
+            "form": NewSearchForm(),
     })
+
+
+def search_form(request):
+    if request.method == "POST":
+        form = NewSearchForm(request.POST)
+        if form.is_valid():
+            title1 = form.cleaned_data["text_form"]
+            title_name = f"{title1}"
+            text_html = util.get_entry(title1)
+            if text_html is None:
+                lists = []
+                for i in util.list_entries():
+                    if title1 in i:
+                        lists.append(i)
+                return render(request, "encyclopedia/search.html", {
+                    "form": NewSearchForm(),
+                    "lists": lists,
+                    "entries": entries,
+                    "title1": title1,
+                })
+            else:
+                text_html = markdown(open(f"entries/{title1}.md").read())
+                return render(request, "encyclopedia/title.html", {
+                    "title_name": title_name,
+                    "text_html": text_html,
+                    "entries": util.list_entries(),
+                    "form": NewSearchForm(),
+                    "title1": title1,
+                    })
