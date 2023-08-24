90160dd085cd31b78508fca28e9381e520117c8a
magari-GH <magari.study@gmail.com>
2023-08-24 15:06:11 +0200

    Added new path for searching form

--- task2/wiki/encyclopedia/urls.py
+++ task2/wiki/encyclopedia/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:title>", views.title, name="title"),
-    path("search/", views.search, name="search")
+    path("search/", views.search_html, name="search"),
+    path("search1/", views.search_form, name="search1"),
+
 ]
