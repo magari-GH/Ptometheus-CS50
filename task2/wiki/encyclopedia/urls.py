f434ae778eb8018c86dc6b6da3575ac70e552f9a
magari-GH <magari.study@gmail.com>
2023-08-25 09:39:34 +0200

    Added new path for "create" function

--- task2/wiki/encyclopedia/urls.py
+++ task2/wiki/encyclopedia/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create", views.create, name="create"),
     path("<str:title>", views.title, name="title"),
     path("search/", views.search_html, name="search"),
     path("search1/", views.search_form, name="search1"),
-
 ]
