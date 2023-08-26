0ce9dedf42633306cd040f2b7caf1ac5839659a7
magari-GH <magari.study@gmail.com>
2023-08-26 08:42:38 +0200

    Added path to 'view.edit'

--- task2/wiki/encyclopedia/urls.py
+++ task2/wiki/encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("edit/<title>", views.edit, name="edit"),
     path("create", views.create, name="create"),
     path("<str:title>", views.title, name="title"),
     path("search/", views.search_html, name="search"),
