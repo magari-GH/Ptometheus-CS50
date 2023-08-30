fff57d14738ab0e67d6a382ea98c91f4df3dfb41
magari-GH <magari.study@gmail.com>
2023-08-30 11:18:33 +0200

    Added model class Auction and its fields (with parameters): title, category, description, image_url, price, owner, winner, is_active. Added model class Bet and its fields (with parameters): title, user, price Added model Comment and its fields (with parameters): title, user, comments. Added model class Watchlist (temporary?) and its fields (with parameters): user, title. This for have table with ManyToManyField relations.

--- task3/commerce/auctions/models.py
+++ task3/commerce/auctions/models.py
@@ -4,3 +4,33 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Auction(models.Model):
+    title = models.CharField(
+        unique=True, error_messages={'unique': "The auction with that title already exists."},
+        max_length=64, verbose_name='title of auction')
+    category = models.CharField(max_length=64)
+    description = models.CharField(blank=True, max_length=124)
+    image_url = models.CharField(blank=True, max_length=124)
+    price = models.FloatField(default=0.0)
+    owner = models.CharField(max_length=64)  # user
+    winner = models.CharField(blank=True, max_length=64)  # user
+    is_active = models.BooleanField(default=False, verbose_name='active status')  # active or non_active
+
+
+class Bet(models.Model):
+    title = models.CharField(max_length=64)  # auction
+    user = models.CharField(max_length=64)  # user
+    price = models.FloatField()
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=64)  # auction
+    user = models.CharField(max_length=64)  # user
+    comments = models.CharField(max_length=128)
+
+
+class Watchlist(models.Model):
+    user = models.CharField(max_length=64)  # user
+    title = models.CharField(max_length=64)  # auction
