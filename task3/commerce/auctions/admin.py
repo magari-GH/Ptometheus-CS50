53d1d568a71d7f5248fefc89831b66416103a165
magari-GH <magari.study@gmail.com>
2023-08-31 13:08:04 +0200

    Added new classes UserAdmin, AuctionAdmin, BetAdmin, CommentAdmin, WatchlistAdmin. After I added those classes to functions admin.site.register. It allows to display information about object for 'superuser'.

--- task3/commerce/auctions/admin.py
+++ task3/commerce/auctions/admin.py
@@ -3,8 +3,28 @@ from .models import User, Auction, Bet, Comment, Watchlist
 # Register your models here.
 
 
-admin.site.register(User)
-admin.site.register(Auction)
-admin.site.register(Bet)
-admin.site.register(Comment)
-admin.site.register(Watchlist)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name')
+
+
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'price', 'is_active', 'owner', 'winner')
+
+
+class BetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'price')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'comment')
+
+
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Auction, AuctionAdmin)
+admin.site.register(Bet, BetAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Watchlist, WatchlistAdmin)
