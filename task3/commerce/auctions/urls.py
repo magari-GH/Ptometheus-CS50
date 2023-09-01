793ba8c2c193ea84a5013835c9c7e3e189217db6
magari-GH <magari.study@gmail.com>
2023-09-01 20:00:15 +0200

    Added new path for view "new_auction"

--- task3/commerce/auctions/urls.py
+++ task3/commerce/auctions/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:auction_id>", views.auction, name="auction"),
+    path("new_auction/", views.new_auction, name="new_auction"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
