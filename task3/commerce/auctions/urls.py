cb8bbb59d425cefc9b25a059bc081a74ad2fb2cf
magari-GH <magari.study@gmail.com>
2023-09-01 16:43:21 +0200

    Added new path for represent page of auction by auction id

--- task3/commerce/auctions/urls.py
+++ task3/commerce/auctions/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:auction_id>", views.auction, name="auction"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
