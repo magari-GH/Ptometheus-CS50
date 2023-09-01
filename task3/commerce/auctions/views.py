fcbee372e02f0fc61b8d9b9c3244d1fea0f4d2e6
magari-GH <magari.study@gmail.com>
2023-09-01 18:30:10 +0200

    Added "new_auction" function. This function create views of new_auction form. Added decorator @login_required for limit access only for logined users

--- task3/commerce/auctions/views.py
+++ task3/commerce/auctions/views.py
@@ -1,15 +1,28 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
+from .forms import AuctionForm
 from .models import User, Auction, Bet, Comment, Watchlist
 
 
 def index(request):
     return render(request, "auctions/index.html", {
         'auctions': Auction.objects.filter(is_active=True)
+    })
+
+
+@login_required
+def new_auction(request):
+    form = AuctionForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = AuctionForm()
+        return HttpResponseRedirect(reverse(index))
+    return render(request, "auctions/new_auction.html", {
+        'form': form
     })
 
 
