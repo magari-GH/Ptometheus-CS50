f31b0b48d193ab216e7ae59c8048be54de390914
magari-GH <magari.study@gmail.com>
2023-09-05 23:26:00 +0200

    Created path for view watchlist_view and watchlist_add

--- task3/commerce/auctions/urls.py
+++ task3/commerce/auctions/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:auction_id>", views.auction, name="auction"),
-    path("new_auction/", views.new_auction, name="new_auction"),
-    path("<int:auction_id>/new_comment/", views.new_comment, name="new_comment"),
+    path("<int:auction_id>", views.auction_view, name="auction"),
+    path("new_auction/", views.new_auction_view, name="new_auction"),
+    path("watchlist_add/<int:auction_id>", views.watchlist_add, name="watchlist_add"),
+    path("watchlist", views.watchlist_view, name="watchlist"),
+    path("<int:auction_id>/new_comment/", views.new_comment_view, name="new_comment"),
     path("categories/", views.categories_view, name="categories"),
     path("categories/category/<str:category>/", views.category_view, name="category"),
     path("login", views.login_view, name="login"),
