af59e7aa554baa0b5bf64f686555942a238a96d7
magari-GH <magari.study@gmail.com>
2023-09-11 18:50:04 +0200

    Added widgets for forms

--- task3/commerce/auctions/forms.py
+++ task3/commerce/auctions/forms.py
@@ -5,6 +5,9 @@ from django import forms
 from .models import Auction, Comment, Bet
 
 
+INPUT_CLASSES = 'w-full py-2 px-6 rounded-xl border'
+
+
 class AuctionForm(forms.ModelForm):
     class Meta:
         model = Auction
@@ -14,25 +17,67 @@ class AuctionForm(forms.ModelForm):
             "description",
             "image_url",
             "price",
-            "owner",
+            # "owner",
         ]
+        widgets = {
+            "title": forms.TextInput(attrs={
+                "class": INPUT_CLASSES
+            }),
+            "category": forms.TextInput(attrs={
+                "class": INPUT_CLASSES
+            }),
+            "description": forms.Textarea(attrs={
+                "class": INPUT_CLASSES
+            }),
+            "image_url": forms.TextInput(attrs={
+                "class": INPUT_CLASSES
+            }),
+            "price": forms.TextInput(attrs={
+                "class": INPUT_CLASSES
+            }),
+            # "owner": forms.Select(attrs={
+            #     "class": INPUT_CLASSES
+            # }),
+        }
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = [
-            'title',
-            'user',
+            # 'title',
+            # 'user',
             'comment',
         ]
+        widgets = {
+            # "title": forms.Select(attrs={
+            #     "class": INPUT_CLASSES
+            # }),
+            # "user": forms.Select(attrs={
+            #     "class": INPUT_CLASSES
+            # }),
+            "comment": forms.TextInput(attrs={
+                "class": INPUT_CLASSES
+            }),
+        }
 
 
 class BetForm(forms.ModelForm):
     class Meta:
         model = Bet
         fields = [
-            'title',
-            'user',
+            # 'title',
+            # 'user',
             'price',
         ]
+        widgets = {
+            # "title": forms.Select(attrs={
+            #     "class": INPUT_CLASSES
+            # }),
+            # "user": forms.Select(attrs={
+            #     "class": INPUT_CLASSES
+            # }),
+            "price": forms.TextInput(attrs={
+                "class": INPUT_CLASSES
+            }),
+        }
