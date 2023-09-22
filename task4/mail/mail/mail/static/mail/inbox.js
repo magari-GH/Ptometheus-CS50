49049641603062fc4773400fd8d97770d1a5c4c6
magari-GH <magari.study@gmail.com>
2023-09-22 10:10:04 +0200

    Added code for representation list of emails. Added function for representation content of email

--- task4/mail/mail/mail/static/mail/inbox.js
+++ task4/mail/mail/mail/static/mail/inbox.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
         load_mailbox('sent');
       });
       return false;
-      load_mailbox('sent');
+      
   };
 
   // Use buttons to toggle between views
@@ -31,6 +31,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function compose_email() {
 
   // Show compose view and hide other views
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
@@ -44,9 +45,67 @@ function compose_email() {
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-}
+ 
+
+  // Show messages
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+      // Вивести листи в консоль
+      console.log(emails);
+      // ... зробити з листами щось інше ...
+      emails.forEach(email => {
+        const element = document.createElement('div');
+        element.style.border = "1px solid";
+        element.style.padding = "5px";
+        element.style.backgroundColor = "lightgray";
+        const id = email.id;
+        const sender = email.sender;
+        const recipients = email.recipients; 
+        const subject = email.subject;
+        const body = email.body;
+        const timestamp = email.timestamp;
+        const read = email.read;
+        const archived = email.archived;
+        // element.innerHTML = `Message from ${sender} about ${subject} recieved ${timestamp}`
+        element.innerHTML = ` ${id} ${sender} ${recipients} ${subject} ${body} ${timestamp} ${read} ${archived}`
+        
+        // function for email representation
+        element.addEventListener('click', function() {
+          console.log(id);
+          document.querySelector('#email-view').style.display = 'block';
+          document.querySelector('#emails-view').style.display = 'none';
+          document.querySelector('#compose-view').style.display = 'none';
+            const element_sender = document.createElement('div');
+            element_sender.innerHTML = `<b>From: </b> ${sender}`;
+            const element_recipients = document.createElement('div');
+            element_recipients.innerHTML = `<b>To: </b> ${recipients}`
+            const element_subgect = document.createElement('div');
+            element_subgect.innerHTML = `<b>Subject: </b> ${subject}`;
+            const element_timestamp = document.createElement('div');
+            element_timestamp.innerHTML = `<b>Timestamp: </b>: ${timestamp} <hr>`;
+            const element_body= document.createElement('deiv');
+            element_body.innerHTML = body;
+
+            document.querySelector('#email-view').append(element_sender, element_recipients, element_subgect, element_timestamp, element_body);
+           
+          
+            
+          
+          
+        
+          });
+        // console.log(sender, subject, timestamp);
+        document.querySelector('#emails-view').append(element);
+      });
+
+    });
+
+
+};
