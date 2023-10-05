8a658a574cad6331e99ca7e3eba7b4d0d653cb09
magari-GH <magari.study@gmail.com>
2023-10-05 08:45:50 +0200

    To file index.js added the function which loads the view "all post" by default. To the element "form" added listener "onsubmit" and function fetch with method "POST" for posting the inputted content from the form to the server.

--- task5/network/project4/network/static/network/index.js
+++ task5/network/project4/network/static/network/index.js
@@ -1,7 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#user-page').addEventListener('click', () => user_page_display());
-    document.querySelector('#all-posts').addEventListener('click', () => all_posts_display());
-    document.querySelector('#following').addEventListener('click', () => following_display());
+
+  document.querySelector('#user-page').addEventListener('click', () => user_page_display());
+  document.querySelector('#all-posts').addEventListener('click', () => all_posts_display());
+  document.querySelector('#following').addEventListener('click', () => following_display());
+  all_posts_display();
+    
+    
+    document.querySelector('form').onsubmit = () => {
+    const publicationUrl = '/compose';
+    const body = document.querySelector('#compose-body').value;
+    const bodyObject = { body };
+
+    fetch(publicationUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify(bodyObject)
+      })
+
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+    })
+
+    .catch(error => {
+      console.log(error);
+    });
+
+    return false;
+  };
+
+
 });
 
 function user_page_display() {
