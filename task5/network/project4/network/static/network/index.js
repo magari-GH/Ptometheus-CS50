dfdd51832fc814956f147625fc2af684a5f4269a
magari-GH <magari.study@gmail.com>
2023-10-06 09:13:15 +0200

    The functions fetch were added to the functions "user_page_display" and "all_posts_display". They allow make a request to the database and get a response with existing publications. Then the publication's details are displayed to a user. Also, the statements for cleaning the divs before displaying the new information were added to those functions.

--- task5/network/project4/network/static/network/index.js
+++ task5/network/project4/network/static/network/index.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelector('#user-page').addEventListener('click', () => user_page_display());
-  document.querySelector('#all-posts').addEventListener('click', () => all_posts_display());
-  document.querySelector('#following').addEventListener('click', () => following_display());
-  all_posts_display();
+  document.querySelector('#all-posts').addEventListener('click', () => all_posts_display('all'));
+  document.querySelector('#following').addEventListener('click', () => following_display('following'));
+  all_posts_display('all');
     
     
     document.querySelector('form').onsubmit = () => {
@@ -38,16 +38,82 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-function user_page_display() {
+function user_page_display(tab) {
     document.querySelector('#user-page-view').style.display = 'block';
     document.querySelector('#all-posts-view').style.display = 'none';
-    document.querySelector('#following-view').style.display = 'none';   
+    document.querySelector('#following-view').style.display = 'none';
+    document.querySelector('#user-page-view').innerHTML = ""; 
+
+    
+    fetch(`/${tab}`)
+    .then(response => response.json())
+    .then(publications => {
+      console.log(publications);
+
+      publications.forEach(publication => {
+        const element = document.createElement('div');
+        element.setAttribute('class', 'div_publication');
+        element.style.border = '1px solid';
+        element.style.padding = "5px";
+        element.style.marginBottom = "10px";
+
+          const publication_user = `<strong> ${publication.user} </strong> <hr>`;          
+          const publication_body = `${publication.body} <hr>`;
+          const publication_timestamp = `<small> ${publication.timestamp} </small> <br>`;
+          const publication_like = `Likes:${publication.like}`;
+
+        element.innerHTML = `${publication_user} ${publication_body} ${publication_timestamp} ${publication_like}`;
+        document.querySelector('#user-page-view').append(element);
+
+      });
+
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    
 };
 
-function all_posts_display() {
+function all_posts_display(tab) {
     document.querySelector('#user-page-view').style.display = 'none';
     document.querySelector('#all-posts-view').style.display = 'block';
-    document.querySelector('#following-view').style.display = 'none'; 
+    document.querySelector('#following-view').style.display = 'none';
+    document.querySelector('#all_element_view').innerHTML = "";
+
+    
+    fetch(`/${tab}`)
+      .then(response => response.json())
+      .then(publications => {
+        console.log(publications);
+
+        publications.forEach(publication => {
+          const element = document.createElement('div');
+          element.setAttribute('class', 'div_publication');
+          element.style.border = '1px solid';
+          element.style.padding = "5px";
+          element.style.marginBottom = "10px";
+
+            const publication_user = `<strong> ${publication.user} </strong> <hr>`;          
+            const publication_body = `${publication.body} <hr>`;
+            const publication_timestamp = `<small> ${publication.timestamp} </small> <br>`;
+            const publication_like = `Likes:${publication.like}`;
+
+          element.innerHTML = `${publication_user} ${publication_body} ${publication_timestamp} ${publication_like}`;
+          document.querySelector('#all_element_view').append(element);
+
+          element.querySelector('strong').addEventListener('click', () => {
+            user_page_display(publication.user);
+            // user_page_display('user5');s
+          })
+
+        });
+
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+
 };
 
 function following_display() {
@@ -55,3 +121,4 @@ function following_display() {
     document.querySelector('#all-posts-view').style.display = 'none';
     document.querySelector('#following-view').style.display = 'block';   
 };
+
