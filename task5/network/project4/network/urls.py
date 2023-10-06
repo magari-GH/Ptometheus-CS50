7ba9c29dbe5dd51f360cd9fc230421042e4f24b3
magari-GH <magari.study@gmail.com>
2023-10-06 08:55:59 +0200

    The new path "represent" was added to the file urls.py. It works with the function fetch to represent the publication. The route is an attribute that passes from fetch to view represent

--- task5/network/project4/network/urls.py
+++ task5/network/project4/network/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("compose", views.compose, name="compose")
+    path("compose", views.compose, name="compose"),
+    path("<str:tab>", views.represent, name="represent"),
 ]
