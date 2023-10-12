62271b29968288f40307d0e30e1dc40a189b33fa
magari-GH <magari.study@gmail.com>
2023-10-12 09:33:24 +0200

    Was created a new path "follow". It is necessary for the realization function the follow the users, create follow and remove it.

--- task5/network/project4/network/urls.py
+++ task5/network/project4/network/urls.py
@@ -4,10 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("follow", views.follow, name="follow"),
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("compose", views.compose, name="compose"),
     path("<str:tab>", views.represent, name="represent"),
+
 ]
