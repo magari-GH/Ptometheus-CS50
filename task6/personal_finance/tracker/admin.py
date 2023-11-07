2186844a42d4a64aeee4441782300d0be1bc8f8d
magari-GH <magari.study@gmail.com>
2023-11-07 16:01:19 +0100

    Added models Transaction, User, Acount to the admin mode

--- task6/personal_finance/tracker/admin.py
+++ task6/personal_finance/tracker/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Transaction, User, Account
+
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email')
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'category', 'title', 'amount', 'currency', 'date')
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'amount', 'currency')
+
+
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Account, AccountAdmin)
